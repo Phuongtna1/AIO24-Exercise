ac432673f065ec008a00c300815e8878cdcc19f1
EmmaPhuongNguyen <phuongtna1@gmail.com>
2024-06-09 01:11:27 +0700

    M01W02 - Done all except Levenshtein distance

---
+++ Module01/Week_2/M01W02_Exercise/question14.py
@@ -0,0 +1,9 @@
+def my_function(x):
+    string=''
+    for i in range(len(x)-1,-1,-1):
+        string += x[i]
+    return string
+x = 'I can do it'
+assert my_function(x) == "ti od nac I"
+x = 'apricot'
+print(my_function(x))
---
+++ Module01/Week_2/M01W02_Exercise/question4.py
@@ -0,0 +1,6 @@
+def levenshtein_distance(token1 , token2):
+
+    return distance
+
+assert levenshtein_distance("hi", "hello") == 4.0
+print(levenshtein_distance("hola", "hello"))
