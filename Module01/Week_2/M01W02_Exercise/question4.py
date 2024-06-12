6d70cb41c02556e1f91b65046472a72659677b8a
EmmaPhuongNguyen <phuongtna1@gmail.com>
2024-06-12 13:16:56 +0700

    <sonarlint check>[fix code]: <fixed all code to fit sonar rules>

--- Module01/Week_2/M01W02_Exercise/question4.py
+++ Module01/Week_2/M01W02_Exercise/question4.py
@@ -1,9 +1,10 @@
-def levenshtein_distance(token1 , token2):
-    change_part = min(len(token1),len(token2))
+def levenshtein_distance(token1, token2):
+    change_part = min(len(token1), len(token2))
     distance = abs(len(token1)-len(token2))
     for i in range(change_part):
-        distance+= 1 if token1[i]==token2[i] else 0
+        distance += 1 if token1[i] == token2[i] else 0
     return distance
 
-assert levenshtein_distance("hi", "hello") == 4.0
+
+assert levenshtein_distance("hi", "hello") == 4
 print(levenshtein_distance("hola", "hello"))
--- Module01/Week_2/M01W02_Exercise/question5.py
+++ Module01/Week_2/M01W02_Exercise/question5.py
@@ -1,16 +1,16 @@
-def check_the_number(N):
+def check_the_number(n):
     list_of_numbers = []
-    result = ""
     for i in range(1, 5):
         list_of_numbers.append(i)
-    if N in list_of_numbers:
+    if n in list_of_numbers:
         results = "True"
-    if N not in list_of_numbers:
+    if n not in list_of_numbers:
         results = "False"
     return results
 
-N = 7
-assert check_the_number(N) == "False"
-N = 2
-results = check_the_number(N)
+
+n = 7
+assert check_the_number(n) == "False"
+n = 2
+results = check_the_number(n)
 print(results)
